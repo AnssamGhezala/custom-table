11aac13ead69031a0cca5660de76691b96945ef7
Anssam Ghezala <anssam.ghezala@mail.mcgill.ca>
2019-08-08 21:48:38 -0400

    finalize reusable component

--- src/Table/styled.js
+++ src/Table/styled.js
@@ -1,12 +1,19 @@
 import styled from "styled-components";
 
 export const TH = styled.th`
+  padding: 0.75em;
+  height: 100%;
   background-color: ${props => {
-    return props.color;
+    return props.theme === "dark" ? "#212529" : "#e8e5e5fa";
+  }};
+  border-collapse: collapse;
+  border-bottom: 2px solid;
+  border-bottom-color: ${props => {
+    return props.theme === "dark" ? "#dee2e6" : "black";
   }};
   color: ${props => {
-    switch (props.color) {
-      case "black":
+    switch (props.theme) {
+      case "dark":
         return "white";
       default:
         return "black";
@@ -14,20 +21,66 @@ export const TH = styled.th`
   }};
 `;
 
-export const TR = styled.tr``;
+export const TR = styled.tr`
+  background-color: ${props => {
+    if (props.isEditing) {
+      return "grey";
+    }
+  }};
+`;
 
 export const TD = styled.td`
-  border: 1px solid #dddddd;
-  text-align: left;
+  border-top: 1px solid #dee2e6;
+  text-align: center;
   padding: 8px;
+  background-color: ${props => {
+    switch (props.theme) {
+      case "dark":
+        return "#212529";
+      default:
+        return "white";
+    }
+  }};
 `;
 
 export const Table = styled.table`
-  font-family: "Roboto", "Helvetica", "Arial", sans-serif;
+  table-layout: fixed;
   font-weight: 400;
   border-collapse: collapse;
   width: 100%;
   display: table;
-  border-style: solid;
-  border-color: black;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.1);
+  color: ${props => {
+    switch (props.theme) {
+      case "dark":
+        return "white";
+      default:
+        return "black";
+    }
+  }};
+  border-bottom: ${props => {
+    switch (props.theme) {
+      case "dark":
+        return "white";
+      default:
+        return "black";
+    }
+  }};
+  height: ${props => {
+    switch (props.density) {
+      case "low":
+        return "90vh";
+      case "avg":
+        return "45vh";
+      case "high":
+        return "5px";
+      default:
+        return "50vh";
+    }
+  }};
+`;
+
+export const Label = styled.label`
+  width: 100%;
+  height: 100%;
 `;
--- src/index.js
+++ src/index.js
@@ -6,21 +6,32 @@ import Table from "./Table/Table";
 import "./styles.css";
 
 function App() {
+  const data = [
+    {
+      Name: "Anssam",
+      Age: "20"
+    },
+    {
+      Name: "Rihab",
+      Age: "12"
+    },
+    {
+      Name: "Amir",
+      Age: "3"
+    }
+  ];
+
   return (
     <div className="App">
-      <Table>
-        <thead>
-          <tr>
-            <Table.TH color="black">Column 1</Table.TH>
-            <Table.TH color="black">Column 2</Table.TH>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>2</td>
-          </tr>
-        </tbody>
+      <Table
+        data={data}
+        theme={Table.THEME.DARK}
+        density={Table.DENSITY.AVERAGE}
+      >
+        <Table.TR>
+          <Table.TD>Seif</Table.TD>
+          <Table.TD>3</Table.TD>
+        </Table.TR>
       </Table>
     </div>
   );
